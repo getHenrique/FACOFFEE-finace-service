688b070817a67c70188a404d45dacd5fb84c4151
Henrique (º-º) <87720975+getHenrique@users.noreply.github.com>
2026-06-05 18:41:24 -0400

    feature (gerar pendencia) - Tradutor de mensagem para DTO configurado

---
+++ src/main/java/com/facoffee/financeService/config/RabbitMQConfig.java
@@ -0,0 +1,15 @@
+package com.facoffee.financeService.config;
+
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+}
