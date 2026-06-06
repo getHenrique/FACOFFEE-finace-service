40797cf0b22c23959d4e72ffe8f7a49b2e0b19b1
Henrique (º-º) <87720975+getHenrique@users.noreply.github.com>
2026-06-06 06:53:15 -0400

    feature (prazos) - Processamento de prazos de pendências implementado

--- src/main/java/com/facoffee/financeService/repository/PendencyRepository.java
+++ src/main/java/com/facoffee/financeService/repository/PendencyRepository.java
@@ -1,8 +1,12 @@
 package com.facoffee.financeService.repository;
 
 import com.facoffee.financeService.entities.Pendency;
+import com.facoffee.financeService.entities.PendencyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface PendencyRepository extends JpaRepository<Pendency, String> {
@@ -10,5 +14,7 @@ public interface PendencyRepository extends JpaRepository<Pendency, String> {
     boolean existsByEventId(String eventId); // Verifica se a mensagem exata do RabbitMQ já foi processada anteriormente
 
     boolean existsByUserIdAndCycle(String userId, String cycle); // Verifica se o participante já foi cobrado neste ciclo específico
+
+    List<Pendency> findAllByStatusAndDueToBefore(PendencyStatus status, LocalDate currentDate); // Lista todas as pendências de certo status com data de vencimento anterior à fornecida
 
 }
